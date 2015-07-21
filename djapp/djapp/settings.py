fdb0ec48499e4f7c7199dff905ab3f65ee3b681e
zdimon <zdimon77@gmail.com>
2015-07-21 16:03:13 +0300

    auto commit

--- djapp/djapp/settings.py
+++ djapp/djapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat',
+
 )
 
 MIDDLEWARE_CLASSES = (
--- djapp/djapp/urls.py
+++ djapp/djapp/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
+
 urlpatterns = [
     # Examples:
     url(r'^$', 'chat.views.home', name='home'),
