4d7a63d1fbf97a5125a6747287a1b19d87827300
zdimon <zdimon77@gmail.com>
2015-08-03 16:53:49 +0300

    auto

--- djapp/djapp/_local.py
+++ djapp/djapp/_local.py
@@ -1,2 +1,17 @@
 SS_SERVER = 'chat.localhost'
 TPA_SERVER = 'chat.localhost'
+
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'brides',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '1q2w3e',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',
+    }
+
+}
+
