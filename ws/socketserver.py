f81ea338045cd3f6a92cc8bae59163817049f6b8
zdimon <zdimon77@gmail.com>
2015-08-03 18:30:28 +0300

    auto

--- ws/socketserver.py
+++ ws/socketserver.py
@@ -130,7 +130,7 @@ application = tornado.web.Application([
  
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(8889)
     myIP = socket.gethostbyname(socket.gethostname())
     print '*** Websocket Server Started at %s***' % myIP
     tornado.ioloop.IOLoop.instance().start()
