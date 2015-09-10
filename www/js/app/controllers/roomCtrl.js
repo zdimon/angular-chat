3b1ad659181026cfb404198cf8f7e090958f28c3
zdimon <zdimon77@gmail.com>
2015-09-10 15:45:42 +0300

    auto

--- www/js/app/controllers/roomCtrl.js
+++ www/js/app/controllers/roomCtrl.js
@@ -222,7 +222,9 @@ app.controller('RoomCtrl', function ($scope, WS, Room, $rootScope, GoogleTransla
            
            if($rootScope.gender=='w') {
 
-                
+                // make a sound
+                alert('saaaaaa');
+                document.getElementById('audio_alert').play();
                 
                
                 // mark user as watching in contact list
@@ -247,9 +249,7 @@ app.controller('RoomCtrl', function ($scope, WS, Room, $rootScope, GoogleTransla
         
          $scope.$on('i_stopted_watching_you', function (event, data) {
            if($rootScope.gender=='w') {
-                // make a sound
-                
-                document.getElementById('audio_alert').play();
+                log(data);
 
                 // mark user as not watching in contact list
                 delete $rootScope.men_watching['user_'+data.user_id];
