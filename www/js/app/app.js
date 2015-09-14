297dc3e0c6f58edce159231db34e7354541acacd
zdimon <zdimon77@gmail.com>
2015-09-14 16:07:57 +0300

    auto

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -72,7 +72,7 @@ The function :func:`someService` does a some function.
 
                         Auth.initialization($stateParams.opponent,function(result){
                             $rootScope.contact_user_list = result.contact.user_list;
-                            $rootScope.online_user_list = esult.online.user_list;
+                            $rootScope.online_user_list = result.online.user_list;
                             $rootScope.online = {};
                             for (var i = 0; i < result.online.user_list.length; i++) {
                                 $rootScope.online['user_'+result.online.user_list[i].user_id] = true;
