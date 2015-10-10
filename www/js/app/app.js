9f3a84fbb2e6afa295cced7194c3a0dcb76c5cbf
zdimon <zdimon77@gmail.com>
2015-10-10 10:54:15 +0300

    auto

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -140,17 +140,3 @@ The function :func:`someService` does a some function.
 
 })
 
-.directive('a', function() {
-    return {
-        restrict: 'E',
-        link: function(scope, elem, attrs) {
-            if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
-                elem.on('click', function(e){
-                    e.preventDefault();
-                    
-                });
-            }
-        }
-   };
-});
-
