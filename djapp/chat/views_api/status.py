8f156a43a9780eb70c28c35f28724c2f8958696d
zdimon <zdimon77@gmail.com>
2015-10-27 14:20:15 +0200

    auto

--- djapp/chat/views_api/status.py
+++ djapp/chat/views_api/status.py
@@ -22,13 +22,13 @@ def set_connected(request,app_name,user_id,source):
     user = ChatUser.objects.get(tpa=tpa,user_id=user_id)
     user.is_online = 1
     user.save()
-    if(source=='chat_side'):
-        mes1 = {'action': 'update_users_online'}
-        mes2 = {'action': 'set_me_online', 'uid': user_id}
-        for u in ChatUser.objects.filter(is_online=1).exclude(user_id=user_id):
-        #for u in ChatUser.objects.all().exclude(user_id=user_id):
-            bclient.publish('%s_%s' % (app_name,u.user_id), json.dumps(mes1))
-            bclient.publish('%s_%s' % (app_name,u.user_id), json.dumps(mes2))
+    #if(source=='chat_side'):
+    mes1 = {'action': 'update_users_online'}
+    mes2 = {'action': 'set_me_online', 'uid': user_id}
+    for u in ChatUser.objects.filter(is_online=1).exclude(user_id=user_id):
+    #for u in ChatUser.objects.all().exclude(user_id=user_id):
+        bclient.publish('%s_%s' % (app_name,u.user_id), json.dumps(mes1))
+        bclient.publish('%s_%s' % (app_name,u.user_id), json.dumps(mes2))
         
     # TODO
     bd.update('update users set online=1 where login=%s' % user_id)
