d5b919763bd7eb4a4e8ede509f19a52b290a6c8f
zdimon <zdimon77@gmail.com>
2016-02-09 10:25:39 +0200

    auto commit

--- www/js/app/tpapp.js
+++ www/js/app/tpapp.js
@@ -53,7 +53,7 @@ tpapp.js
          
 
             $rootScope.$on('show_new_message_notification',function(event,data){
-                
+                   alert('show_new_message_notification');
                    if(typeof $rootScope.notifies[data] === 'undefined') {
                     if(!$rootScope.active_contacts['user_'+data.opponent.user_id]){
                         if($rootScope.gender=='w'){ document.getElementById('audio_alert').play(); }
@@ -65,6 +65,7 @@ tpapp.js
             });
 
             $rootScope.$on('show_invite_notification',function(event,data){
+                alert('show_invite_notification');
                 if(typeof $rootScope.notifies[data.data.id] === 'undefined') {
                     if(!$rootScope.active_contacts['user_'+data.data.opponent.user_id]){
                         if($rootScope.gender=='w'){document.getElementById('audio_alert').play(); }
@@ -75,6 +76,7 @@ tpapp.js
             });
 
             $rootScope.$on('show_multi_invite_notification',function(event,data){
+                alert('show_multi_invite_notification');
                 if(typeof $rootScope.notifies[data.data.id] === 'undefined') {
                     if(!$rootScope.active_contacts['user_'+data.data.opponent.user_id]){
                         if($rootScope.gender=='w'){document.getElementById('audio_alert').play(); }
