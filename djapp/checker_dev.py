12ec56bbc0cd241f5cda28fe869c85f62c0d59a7
zdimon <zdimon77@gmail.com>
2016-04-20 13:15:19 +0300

    auto commit

--- djapp/checker_dev.py
+++ djapp/checker_dev.py
@@ -12,40 +12,37 @@ import datetime
 
 
 while True:
-
-    time.sleep(10) 
+    connect_timeout = 1
+    time.sleep(30) 
     print 'check'
    
     Command_reload = '/home/webmaster/ngchat_ve/angular-chat/djapp/kill_ws.sh'
 
-    url2 = 'http://marriage-brides.com:8882/api/tpa1com/config.js'
-    rez = requests.get(url2)
-    if rez.status_code != 200:
-        call(Command_reload) 
-        print 'restarting'   
-        f = open('restart.log','a')
-        f.write('reloading ws - %s' % datetime.datetime.now() )
-        f.close()
+    
+    for p in [8882,8883,8884]:
+        url2 = 'http://marriage-brides.com:%s/api/tpa1com/config.js' % p
+        rez = requests.get(url2)
+        print 'check %s post om 200' % p
+        if rez.status_code != 200:
+            call(Command_reload) 
+            print 'restarting'   
+            f = open('restart.log','a')
+            f.write('reloading ws - %s' % datetime.datetime.now() )
+            f.close()
 
 
-    url2 = 'http://marriage-brides.com:8883/api/tpa1com/config.js'
-    rez = requests.get(url2)
-    if rez.status_code != 200:
-        call(Command_reload) 
-        print 'restarting'   
-        f = open('restart.log','a')
-        f.write('reloading ws - %s' % datetime.datetime.now() )
-        f.close()
 
-    url2 = 'http://marriage-brides.com:8884/api/tpa1com/config.js'
-    rez = requests.get(url2)
-    if rez.status_code != 200:
-        call(Command_reload) 
-        print 'restarting'   
-        f = open('restart.log','a')
-        f.write('reloading ws - %s' % datetime.datetime.now() )
-        f.close()
-
+    for p in [8882,8883,8884]
+        url = 'http://marriage-brides.com:%s/api/tpa1com/config.js' % p
+        print 'check %s post on timeout' % p
+        try:
+            rez = requests.get(url,timeout=(connect_timeout, 5))
+        except:
+            print 'killing websocket becouse timeout!!'
+            call(Command_reload)
+            f = open('restart.log','a')
+            f.write('reloading becouse timeout ws - %s' % datetime.datetime.now() )
+            f.close()
 
 
 
